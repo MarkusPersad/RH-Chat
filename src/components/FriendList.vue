40ba65724866bad71da8b3cedc90a1fa83138c1b
MarkusPersad <msp060308@gmail.com>
2025-12-10 16:06:08 +0800

    chore(deps): 更新多个依赖包版本
    - 将 @tauri-apps/cli 从 2.9.5 更新至 2.9.6
    - 将 daisyui 从 5.5.5 更新至 5.5.8
    - 将 vite 从 7.2.6 更新至 7.2.7
    - 将 vue-tsc 从 3.1.5 更新至 3.1.8
    - 更新 Rust 依赖包 cc、dlopen2、hyper-util 等至最新版本
    - 升级 libc、log、mio、reqwest、toml_edit 等 crates 的版本
    - 更新 tauri 及其运行时相关组件到最新补丁版本

--- src/components/FriendList.vue
+++ src/components/FriendList.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
 </script>
 <template>
+    <ul class="list">
+        <li class=" list-row p-4 pb-2 text-2xl opacity-60 tracking-wide">
+            <div class="iconfont icon-biaoqing"></div>
+            <span>好友</span>
+        </li>           
+    </ul>
 </template>
